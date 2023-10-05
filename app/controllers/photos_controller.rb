98df33a0a1f9194463c4a11e8d916f8c9e86b421
Ethan Huang <96758695+EthanKH@users.noreply.github.com>
2023-10-05 21:58:21 +0000

    new user form and new comments needed

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -4,12 +4,14 @@ class PhotosController < ApplicationController
     @photos_list = matching_photos.order({ :created_at => :desc })
     render({ :template => "photo_templates/index" })
   end
+
   def details
     url_photo = params.fetch("path_photo")
     matching_photos = Photo.where({ :id => url_photo })
     @the_photo = matching_photos.first
     render({ :template => "photo_templates/details" })
   end
+
   def create
     input_photo = params.fetch("query_photo")
     input_caption = params.fetch("query_caption")
@@ -22,6 +24,7 @@ class PhotosController < ApplicationController
     new_photo.save
     redirect_to("/photos/" + new_photo.id.to_s)
   end
+
   def update
     # Parameters: {"query_image"=>"https://hips.hearstapps.com/hmg-prod/images/dog-puppy-on-garden-royalty-free-image-1586966191.jpg?crop=0.752xw:1.00xh;0.175xw,0&resize=1200:*;set=set1", "query_caption"=>"dog test", "modify_id"=>"956"}
     the_id = params.fetch("modify_id")
@@ -34,8 +37,12 @@ class PhotosController < ApplicationController
     updated_photo.image = input_photo
     updated_photo.caption = input_caption
     updated_photo.save
+    # alt redirect
+    # new_url = "/photos/" + updated_photo.id.to_s
+    # redirect_to(next_url)
     redirect_to("/photos/" + updated_photo.id.to_s)
   end
+  
   def delete
     the_id = params.fetch("delete_id")
     matching_photos = Photo.where({ :id => the_id })
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,11 +4,13 @@ class UsersController < ApplicationController
     @users_list = matching_users.order({ :username => :asc })
     render({ :template => "user_templates/index" })
   end
+
   def index
     matching_users = User.all
     @users_list = matching_users.order({ :username => :asc })
     render({ :template => "user_templates/index" })
   end
+
   def details
     # parameters: {"path_username"=>"anisa"}
     url_username = params.fetch("path_username")
@@ -19,5 +21,11 @@ class UsersController < ApplicationController
     # else
       render({ :template => "user_templates/details" })
     # end
+  end
+
+  def create
+    
+    # redirect_to("/users/" + new_id.to_s)
+    render({ :template => "user_templates/create" })
   end
 end
