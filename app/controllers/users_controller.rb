995b7a99ba577fdda67b0dc8bb6fa8194fde76d4
Ethan Huang <96758695+EthanKH@users.noreply.github.com>
2023-10-05 16:43:17 +0000

    users mostly functional

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,18 @@
+class UsersController < ApplicationController
+  def index
+    matching_users = User.all
+    @users_list = matching_users.order({ :username => :asc })
+    render({ :template => "user_templates/index" })
+  end
+  def details
+    # parameters: {"path_username"=>"anisa"}
+    url_username = params.fetch("path_username")
+    matching_usernames = User.where({ :username => url_username })
+    @the_user = matching_usernames.first
+    # if @the_user == nil
+    #   redirect_to("/404")
+    # else
+      render({ :template => "user_templates/details" })
+    # end
+  end
+end
